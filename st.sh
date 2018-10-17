b12c3cb55559545f8dab1bdd964345b9a356b327
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-10-17 10:55:19 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,8 +1,8 @@
-# 16-10-2018 JHZ
+# 17-10-2018 JHZ
 
 git add README.md
 git commit -m "README"
-for d in INSTALL.md PARALLEL.md st.sh
+for d in INSTALL.md LANGUAGES.md PARALLEL.md SYSTEMS.md st.sh
 do
    git add $d
    git commit -m "$d"
