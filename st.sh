7d30487297e754e5015e370258fd14f39ef9a476
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-05-21 08:57:35 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -2,7 +2,7 @@
 
 git add README.md
 git commit -m "README"
-for d in INSTALL.md LANGUAGES.md PARALLEL.md REPRODUCE.mdSYSTEMS.md st.sh
+for d in INSTALL.md LANGUAGES.md PARALLEL.md REPRODUCE.md SYSTEMS.md st.sh
 do
    git add $d
    git commit -m "$d"
