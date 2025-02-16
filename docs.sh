70bd3abb90204b61df1e95096914edb76f5048ec
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-02-16 23:04:35 +0000

    Deployed 1e691ea with MkDocs version: 1.5.3

--- docs.sh
+++ docs.sh
@@ -24,6 +24,7 @@ function github_pages()
 if [ "$(uname -n | sed 's/-[0-9]*$//')" == "login-q" ]; then
    module load ceuadmin/libssh/0.10.6-icelake
    module load ceuadmin/openssh/9.7p1-icelake
+   module load ceuadmin/krb5
 fi
 
 github_pages
