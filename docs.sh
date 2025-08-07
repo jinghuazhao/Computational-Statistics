b64e53f2465da1eac5cdea4b2d08e6a123398500
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-08-07 16:11:28 +0100

    Deployed e540317 with MkDocs version: 1.5.3

--- docs.sh
+++ docs.sh
@@ -27,7 +27,8 @@ if [ "$(uname -n | sed 's/-[0-9]*$//')" == "login-q" ]; then
    module load ceuadmin/krb5
 fi
 
-export LD_PRELOAD=/usr/lib64/libcrypto.so.1.1
+# cclake
+# export LD_PRELOAD=/usr/lib64/libcrypto.so.1.1
 github_pages
 mkdocs build
 mkdocs gh-deploy
